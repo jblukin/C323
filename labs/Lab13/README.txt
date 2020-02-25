069d9bbc5b58dad198bfd546fc05a00f8450c241
Jonah Lukin <jonahblukin@149-161-201-205.dhcp-bl.indiana.edu>
2020-02-25 17:00:30 -0500

    Lab 13 commit

---
+++ labs/Lab13/README.txt
@@ -0,0 +1,3 @@
+Lab 13 - Jonah Lukin - 2/25/20
+
+The cells are removed and regenerated as they leave and re-enter the view
---
+++ labs/Lab13/lab13/lab13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab13
+//
+//  Created by Jonah Lukin on 2/25/20.
+//  Copyright © 2020 C323 / Spring2020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
